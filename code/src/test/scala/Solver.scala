204f6de25aca593caddf35d8c6dd564dae38294d
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2025-05-13 01:55:48 +0200

    Add testing for UNSAT formulas and theory solvers

--- code/src/test/scala/Solver.scala
+++ code/src/test/scala/Solver.scala
@@ -7,42 +7,56 @@ class SolverTest extends munit.FunSuite {
     "SimpleSAT" -> SimpleSAT,
     "DPLL" -> DPLL,
     "ClausalDPLL" -> ClausalDPLL,
+    "ClausalDPLL(T)" -> ClausalDPLL[Prop](),
     // "CDCL" -> CDCL,
   )
 
-  val theorySolvers: List[(String, TheorySolver[Prop])] = List(
-    "ClausalDPLL(T)" -> ClausalDPLL[Prop]()
-  )
+  val a = Var.next
+  val b = Var.next
+  val c = Var.next
 
   val trueTest = (name: String, solver: Solver[Prop]) => 
     test(s"$name: True is SAT") {
       val f = True
       val result = solver.checkSat(f)
-      assert(result.isSat)
+      assert(result.isSat, s"$name: $f should be SAT")
     }
 
   val falseTest = (name: String, solver: Solver[Prop]) =>
     test(s"$name: False is UNSAT") {
       val f = False
       val result = solver.checkSat(f)
-      assert(!result.isSat)
+      assert(!result.isSat, s"$name: $f should be UNSAT")
     }
 
   val trivialSATFormulas = List(
-    Var.next,
-    Var.next \/ Var.next,
-    Var.next /\ Var.next,
-    !Var.next,
-    Var.next -> Var.next,
-    Var.next <-> Var.next,
-    Var.next <-> (Var.next /\ Var.next),
+    a,
+    a \/ a,
+    a /\ b,
+    !a,
+    b -> a,
+    a <-> b,
+    a <-> (b /\ a),
+  )
+
+  val trivialUNSATFormulas = List(
+    a /\ !a,
+    a <-> !a,
   )
 
   val trivialSATTest = (name: String, solver: Solver[Prop]) =>
     test(s"$name: Trivial SAT formulas") {
       for (f <- trivialSATFormulas) {
         val result = solver.checkSat(f)
-        assert(result.isSat)
+        assert(result.isSat, s"$name: $f should be SAT")
+      }
+    }
+
+  val trivialUNSATTest = (name: String, solver: Solver[Prop]) =>
+    test(s"$name: Trivial UNSAT formulas") {
+      for (f <- trivialUNSATFormulas) {
+        val result = solver.checkSat(f)
+        assert(!result.isSat, s"$name: $f should be UNSAT")
       }
     }
 
@@ -50,7 +64,8 @@ class SolverTest extends munit.FunSuite {
   val tests = List(
     trueTest,
     falseTest,
-    trivialSATTest
+    trivialSATTest,
+    trivialUNSATTest,
   )
 
   for ((name, solver) <- solvers) {
